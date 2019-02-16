382f8a30c36c64a4a6b14ddc52aad199dfb1c0c7
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2019-02-16 16:29:09 +0100

    Added authentication for SPA application - login page - sign up page - disconnect button

--- src/services/identity/Identity.DataStores.SqlServer/Migrations/20190213121048_Account_AddRefreshTokenToken.Designer.cs
+++ src/services/identity/Identity.DataStores.SqlServer/Migrations/20190213121048_Account_AddRefreshTokenToken.Designer.cs
@@ -4,14 +4,16 @@ using Identity.DataStores.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Identity.DataStores.SqlServer.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20190213121048_Account_AddRefreshTokenToken")]
+    partial class Account_AddRefreshTokenToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
